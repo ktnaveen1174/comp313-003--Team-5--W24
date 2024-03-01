d5a1ac62b2874436888af3c5a6965e248821559a
deliciads <delicia1999@yahoo.com>
2024-03-01 00:15:22 -0500

    Re-add FrontEnd contents

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+      
+        <div className="app-container">
+            <h1>Group Five's Software Development Project</h1>
+            <h3>By Haseeb Siddiqi, Abdul Imran, Naveen, Delicia D'Souza, Nilkanth, Sandip, Anthony Dennis, Piyush</h3>
+            <input
+                type="text" placeholder="Questions will go here"
+            />
+            <button >Submit</button>
+        </div>
+    );
+}
+
+
+
+export default App;
