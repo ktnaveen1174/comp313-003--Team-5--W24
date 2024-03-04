ced5985133d1f3d4d98cef52c346ae4bf8919c34
Haseeb Siddiqi <haseebs727@gmail.com>
2024-03-04 17:20:36 -0500

    Took out text field and submit button

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -7,10 +7,7 @@ const App = () => {
         <div className="app-container">
             <h1>Group Five's Software Development Project</h1>
             <h3>By Haseeb Siddiqi, Abdul Imran, Naveen, Delicia D'Souza, Nilkanth, Sandip Mishra, Anthony Dennis, Piyush</h3>
-            <input
-                type="text" placeholder="Questions will go here"
-            />
-            <button >Submit</button>
+          
         </div>
     );
 }
